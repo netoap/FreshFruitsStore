e6a711f8e73bd59583e6e06d25e0d6c517e2e836
DESKTOP-43UUD3H\Alexander <alexander.neto@antartyca.com>
2022-02-13 13:16:49 +0100

    Agregar archivos de proyecto.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FreshFruitsStore.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FreshFruitsStore.Startup))]
+namespace FreshFruitsStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
